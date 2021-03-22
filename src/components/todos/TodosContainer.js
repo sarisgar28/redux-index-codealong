47efbad30470306a878a67d890c0e88ff12a6ee6
sarisgar28 <sarisgar28@gmail.com>
2021-03-22 13:37:18 -0400

    commit tree 1324392782be19249ed52c8751c3f50d12d126d0

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class TodosContainer extends Component {
+
+  render() {
+    return(
+      <div></div>
+    );
+  }
+};
+const mapStateToProps = state => {
+    return {
+      todos: state.todos
+    }
+  }
+  
+  export default connect(mapStateToProps)(TodosContainer); 
